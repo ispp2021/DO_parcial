17f7f79d4e39b25a7b8533d3bd192732b2bb41b7
ispp2021 <ispp.ie@gmail.com>
2024-01-16 16:12:32 -0500

    Archivo import kaggle para descargar dataset

---
+++ import_kaggle.py
@@ -0,0 +1,32 @@
+import json
+import zipfile
+import os
+
+api_token={"username":"ismael2024","key":"2c5411503c8206077bdd38ef933181fe"}
+
+with open('C:/Users/ipacompia/.kaggle/kaggle.json','w') as file:
+    json.dump(api_token,file)
+
+location="C:/Users/ipacompia/Documents/proyecto_parcial/dataset"
+
+#validar que la carpeta exista
+if not os.path.exists(location):
+    #si no existe la carpeta dataset la creo.
+    os.mkdir(location)
+else:
+    #si la carpeta existe, se borra datos para que se pueda cargar datos actualizados
+    for root, dirs, files in os.walk(location, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root,name))  #elimina todos los ficheros
+        for name in dirs:
+            os.rmdir(os.path.join(root,name))   #elimina todas las carpetas
+
+#Descargar dataset de kaggle
+os.system("kaggle datasets download -d henryshan/starbucks -p C:/Users/ipacompia/Documents/proyecto_parcial/dataset")
+#descomprimir el archivo de kaggle
+os.chdir(location)
+for file in os.listdir():               #lee los archivos .zip
+    zip_ref=zipfile.ZipFile(file,"r")   #extrae el contenido
+    zip_ref.extractall()
+    zip_ref.close()                     #cierra archivo
+
